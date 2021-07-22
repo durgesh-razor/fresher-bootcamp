e9d07ab8e1653dafa29d3223def31ea6baee0e09
durgesh-razor <durgesh.kumar@razorpay.com>
2021-07-22 18:53:41 +0530

    day 2

--- day2/q1.go
+++ day2/q1.go
@@ -5,10 +5,10 @@ import (
 	"strings"
 )
 
-func CountCharactersFrequency(str string, results chan<- [26]int) {
-	var cnt [26]int
-	for i := 0; i < 26; i++ {
-		cnt[i] += strings.Count(str, string(rune('a'+i)))
+func CountCharactersFrequency(str string, results chan<- [128]int) {
+	var cnt [128]int
+	for i := 0; i < 128; i++ {
+		cnt[i] += strings.Count(str, string(rune(' '+i)))
 	}
 
 	results <- cnt
@@ -16,22 +16,22 @@ func CountCharactersFrequency(str string, results chan<- [26]int) {
 func main() {
 	numberOfStrings := 2
 	strings := []string{"aaabbc", "aaabbd"}
-	results := make(chan [26]int, numberOfStrings)
+	results := make(chan [128]int, numberOfStrings)
 
 	for stringCounter := 0; stringCounter < numberOfStrings; stringCounter++ {
 		go CountCharactersFrequency(strings[stringCounter], results)
 	}
 
-	var finalFrequency [26]int
+	var finalFrequency [128]int
 
 	for _i := 0; _i < numberOfStrings; _i++ {
 		result := <-results
-		for i := 0; i < 26; i++ {
+		for i := 0; i < 128; i++ {
 			finalFrequency[i] += result[i]
 		}
 	}
 
-	for i := 0; i < 26; i++ {
-		fmt.Printf("%c : %d\n", rune('a'+i), finalFrequency[i])
+	for i := 0; i < 128; i++ {
+		fmt.Printf("%c : %d\n", rune(' '+i), finalFrequency[i])
 	}
 }
